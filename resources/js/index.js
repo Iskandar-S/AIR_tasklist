ff25cecbb97c482dc66cd4eba341a3a1368261e9
Iskandar <isalokhiddinov@gmail.com>
2021-10-29 12:19:56 -0400

    resolved conflict

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,5 +1,11 @@
 console.log("connected!");
-
 const task = new TaskManager();
 console.log(task.tasks);
-
+const submit = document.getElementById('submit');
+const reset = document.getElementById('reset');
+// function showTask() {
+//     const taskPage = document.querySelectorAll('class')
+//     const taPage = new TaskManager();
+//     ShowTask.addTask = (document.querySelectorAll('class'))
+// }
+// submit.onclick = ShowTask;
