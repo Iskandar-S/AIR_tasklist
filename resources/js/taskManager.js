17124afd597dc8d6256e7c3899b0203747355744
Iskandar <30454668+spacefox0731@users.noreply.github.com>
2021-10-29 12:26:47 -0400

    Iskandar (#9)
    * added home page, edited addTask
    
    * check
    
    * completed class, method, assigned buttons
    
    * check
    
    * check
    
    * resolved conflict

--- resources/js/taskManager.js
+++ resources/js/taskManager.js
@@ -1,13 +1,14 @@
 console.log("connected!");
-
 class TaskManager {
-    constructor(project, tasks, description, assignedTo, status, dueDate, currentId){
-        this.project = project;
-        this.tasks = [];
-        this.description = description;
-        this.assignedTo = assignedTo;
-        this.status = status;
-        this.dueDate = dueDate;
+    constructor() {
+    this.tasks = [];
+    this.currentId = 0;
     }
-
+    addTask(name, description, assignedTo, dueDate, status = "TODO") {
+        this.currentId ++;
+        this.tasks.push(this.currentId, name, description, assignedTo, dueDate, status);
+    }
 }
+const testTask = new TaskManager();
+console.log(testTask.tasks);
+testTask.addTask("finish project", 'adsfdsf', 'Nat5', '10-28-2021');
