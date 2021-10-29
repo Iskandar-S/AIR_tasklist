6ef1c271764fc4b52ac1f3d8d7af44a96013a57b
Iskandar <isalokhiddinov@gmail.com>
2021-10-29 11:51:01 -0400

    check

--- resources/js/taskManager.js
+++ resources/js/taskManager.js
@@ -1,23 +1,7 @@
-console.log("connected!");
-
-
-
-
 class TaskManager {
-    constructor() {
-    this.tasks = [];
-    this.currentId = 0;
-    }
+    constructor(tasks,currentId=0) {
+        this.tasks = [];
+        this.currentId = currentId;
+    } 
     
-    addTask(name, description, assignedTo, dueDate, status = "TODO") {
-        this.currentId ++;
-        this.tasks.push(this.currentId, name, description, assignedTo, dueDate, status);
-    }
 }
-
-const testTask = new TaskManager();
-console.log(testTask.tasks);
-testTask.addTask("finish project", 'adsfdsf', 'Nat5', '10-28-2021');
-
-
-
