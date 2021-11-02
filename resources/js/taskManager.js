a5efa0759e43e538520872f34ea1aa078417cf8f
Iskandar <isalokhiddinov@gmail.com>
2021-11-02 15:28:55 -0400

    Created submit form, started validation and render

--- resources/js/taskManager.js
+++ resources/js/taskManager.js
@@ -1,14 +1,62 @@
 console.log("connected!");
+
+const createTaskHtml = (name, description, assignedTo, dueDate, status) => {
+    html = `<h2 class="card-title" >Card#1</h2>
+    <table class="table table-borderless">
+      <tbody>
+        <tr>
+          <td class="fw-bold">Project</td>
+          <td id>${name}</td>
+        </tr>
+        <tr>
+          <td class="fw-bold">Description</td>
+          <td>${description}</td>
+        </tr>
+        <tr>
+          <td class="fw-bold">Assigned To</td>
+          <td >${assignedTo}</td>
+        </tr>
+        <tr>
+          <td class="fw-bold">Due Date</td>
+          <td>${dueDate}</td>
+        </tr>
+        <tr>
+          <td class="fw-bold">Status</td>
+          <td>${status}</td>
+        </tr>
+      </tbody>
+    </table>`;
+    return html;
+};
+
+// Create TaskManager class
 class TaskManager {
-    constructor() {
-    this.tasks = [];
-    this.currentId = 0;
+    constructor(currentId = 0) {
+      this.tasks = [];
+      this.currentId = currentId;
     }
-    addTask(name, description, assignedTo, dueDate, status = "TODO") {
-        this.currentId ++;
-        this.tasks.push(this.currentId, name, description, assignedTo, dueDate, status);
+    // Add addTask method
+    addTask(name, description, assignedTo, dueDate) {
+      const task = {
+        id: this.currentId++,
+        name,
+        description,
+        assignedTo,
+        dueDate,
+        status: 'TODO'
+      };
+      this.tasks.push(task);
+      console.log(this.tasks);
     }
-}
-const testTask = new TaskManager();
-console.log(testTask.tasks);
-testTask.addTask("finish project", 'adsfdsf', 'Nat5', '10-28-2021');
+    // Add render method (In Progress)
+//     render() {
+//       const tasksHtmlList = [];
+
+//       for (let i = 0; i < this.tasks.length; i++) {
+//         const task = this.tasks[i];
+        
+//         const date = new Date(task.dueDate);
+//         const formattedDate = date.
+//       }
+//     }
+};
