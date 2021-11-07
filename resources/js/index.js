7f2a488d5ad762fcc976af8991ccfe3da96e7705
Iskandar <30454668+spacefox0731@users.noreply.github.com>
2021-11-07 11:58:09 -0500

    Iskandar (#18)
    * added home page, edited addTask
    
    * check
    
    * completed class, method, assigned buttons
    
    * check
    
    * check
    
    * resolved conflict
    
    * Created submit form, started validation and render
    
    * check
    
    * added error messages, fixed mark as done and delete buttons

--- resources/js/index.js
+++ resources/js/index.js
@@ -20,29 +20,45 @@ const nameInput = document.querySelector("#name");
 const descriptionInput = document.querySelector("#description");
 const assignedToInput = document.querySelector("#assignedTo");
 const dueDateInput = document.querySelector("#dueDate");
-const statusInput = document.querySelector("#status");
+// const statusInput = document.querySelector("#status");
 const errorMessage = document.querySelector("#alertMessage");
 
 const name = nameInput.value;
 const description = descriptionInput.value;
 const assignedTo = assignedToInput.value;
 const dueDate = dueDateInput.value;
-const status = statusInput.value;
+// const status = statusInput.value;
 
 // Validate (In progress)
-function validate(name, description, assignedTo) {
-if (name.length < 4){
-    errorMessage.innerHTML = "Name must be longer than 4 characters";
-    errorMessage.style.display = "block";
-} else if (description.length < 6) {
-    errorMessage.innerHTML = "Description must be longer than 6 characters";
-    errorMessage.style.display = "block";
-} else if (assignedTo.length < 2) {
-    errorMessage.innerHTML = "Invalid must be longer than 2 characters";
-    errorMessage.style.display = "block";
-} else {
-    errorMessage.style.display = "none";
-};
+// function validFormFieldInput(data) {
+//     const nameInput = document.querySelector("#name");
+//     const descriptionInput = document.querySelector("#description");
+//     const assignedToInput = document.querySelector("#assignedTo");
+//     const dueDateInput = document.querySelector("#dueDate");
+//     const statusInput = document.querySelector("#status");
+
+//     const name = nameInput.value;
+//     const description = descriptionInput.value;
+//     const assignedTo = assignedToInput.value;
+//     const dueDate = dueDateInput.value;
+//     const status = statusInput.value;
+
+//     console.log("name: " + name + " description: " + description + " assignedTo: " + assignedTo + " dueDate: " + dueDate + " status: " + status)
+
+//     data = []
+
+// if (name.length <= 4){
+//     document.querySelector("#errorName").innerHTML = "Name must be longer than 4 characters";
+//     document.querySelector("#errorName").style.display = "block";
+// } else if (description.length <= 6) {
+//     document.querySelector("#errorDescription").innerHTML = "Description must be longer than 6 characters";
+//     document.querySelector("#errorDescription").style.display = "block";
+// } else if (assignedTo.length <= 1) {
+//     document.querySelector("#errorAssignedTo").innerHTML = "Invalid assignee name must be longer than 1 character";
+//     document.querySelector("#errorAssignedTo").style.display = "block";
+// } else {
+//     document.querySelector("#success").style.display = "none";
+// };
 
 // Due Date input checks for data type by default, but we have to check for valid date. Date can't be from past or far future.
 
@@ -53,7 +69,7 @@ if (name.length < 4){
 //     errorMessage.style.display = "none";
 //     return true;
 // }
-};
+// };
 
 // Submit form
 newTaskForm.addEventListener('submit', (event) => {
@@ -66,27 +82,45 @@ newTaskForm.addEventListener('submit', (event) => {
     const description = descriptionInput.value;
     const assignedTo = assignedToInput.value;
     const dueDate = dueDateInput.value;
-    const status = statusInput.value;
+    // const status = statusInput.value;
     
-    validate(name, description, assignedTo);
-    // Add task
-    newTask.addTask(name, description, assignedTo, dueDate, status);
-
-    newTask.save();
-
-    newTask.render();
-
-    nameInput.value = '';
-    descriptionInput.value = '';
-    assignedToInput.value = '';
-    dueDateInput.value = '';
-});
+    //Validate inputs
+    if (name.length <= 4) {
+        document.querySelector("#errorName").style.display = "block";
+        document.querySelector("#success").style.display = "none";
+    } else {
+        document.querySelector("#errorName").style.display = "none";
+    };
+    if (description.length <= 6) {
+        document.querySelector("#errorDescription").style.display = "block";
+        document.querySelector("#success").style.display = "none";
+    } else {
+        document.querySelector("#errorDescription").style.display = "none";
+    };
+    if (assignedTo.length <= 1) {
+        document.querySelector("#errorAssignedTo").style.display = "block";
+        document.querySelector("#success").style.display = "none";
+    } else {
+        document.querySelector("#errorAssignedTo").style.display = "none";
+    };
+    if ((name.length > 4) && (description.length > 6) && (assignedTo.length > 1)) {
+        document.querySelector(".alert-danger").style.display = "none";
+        document.querySelector("#success").style.display = "block";
+        newTask.addTask(name, description, assignedTo, dueDate);
+        newTask.save();
+        newTask.render();
+        nameInput.value = '';
+        descriptionInput.value = '';
+        assignedToInput.value = '';
+        dueDateInput.value = '';
+    };
+    });
 
 const taskCard = document.querySelector('#taskCard');
 
 taskCard.addEventListener('click', (event) => {
-    if (event.target.classList.contains('update-button')) {
-        const parentTask = event.target.parentElement.parentElement;
+    if (event.target.classList.contains('done-button')) {
+        const parentTask = event.target.parentElement.parentElement.parentElement;
 
         const taskId = Number(parentTask.dataset.taskId);
 
@@ -102,7 +136,7 @@ taskCard.addEventListener('click', (event) => {
     // Check if a "Delete" button was clicked
     if (event.target.classList.contains('delete-button')) {
         // Get the parent Task
-        const parentTask = event.target.parentElement.parentElement;
+        const parentTask = event.target.parentElement.parentElement.parentElement;
 
         // Get the taskId of the parent Task.
         const taskId = Number(parentTask.dataset.taskId);
